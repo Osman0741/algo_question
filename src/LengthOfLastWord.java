9ad82eb0768d6a5ea454c9f62b92d44a0c42f25e
LENOVO <osmnozdmrosh@gmail.com>
2023-08-06 13:13:07 +0300

    Length of Last Word

---
+++ src/LengthOfLastWord.java
@@ -0,0 +1,21 @@
+public class LengthOfLastWord {
+
+    public static void main(String[] args) {
+
+        String str="How are you Osman?";
+        System.out.println(lengthOfLastWord(str));
+
+
+    }
+    public static int lengthOfLastWord(String s){
+        s = s.trim();
+        int lastIndex = s.lastIndexOf(' ')+1;
+
+        return s.length() - lastIndex;
+
+
+
+//        String [] arr =s.split(" ");
+//        return arr[arr.length-1].length();
+    }
+}
